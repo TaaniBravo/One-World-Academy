fb7f2d70f751122813844eeb0ec81318fd0aecc1
Taani Maama <taani1maama@live.com>
2021-01-20 18:40:19 -0800

    Updated the if statement.

--- public/assets/js/signup.js
+++ public/assets/js/signup.js
@@ -14,7 +14,12 @@ $(document).ready(() => {
       lastName: lastNameInput.val().trim()
     };
 
-    if (!userData.email || !userData.password) {
+    if (
+      !userData.email ||
+      !userData.password ||
+      !userData.firstName ||
+      !userData.lastName
+    ) {
       return;
     }
     // If we have an email and password, run the signUpUser function
