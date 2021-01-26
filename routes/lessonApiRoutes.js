07c3eec4a5a78fafdcfcaeae6011f667bcaec3ce
Taani Maama <taani1maama@live.com>
2021-01-25 19:19:56 -0800

    Updated the Lesson routes so with PUT and DELETE methods.

--- routes/lessonApiRoutes.js
+++ routes/lessonApiRoutes.js
@@ -1,8 +1,11 @@
 const db = require("../models");
 
 module.exports = app => {
+  // GET Route for viewing all of the users lessons.
   app.get("/api/lessons", async (req, res) => {
-    const lessons = await db.Lesson.findAll({});
+    const lessons = await db.Lesson.findAll({
+      where: { UserId: req.user.id }
+    });
 
     try {
       res.json(lessons);
@@ -11,6 +14,7 @@ module.exports = app => {
     }
   });
 
+  // POST Route for create a user's lesson.
   app.post("/api/lessons", async (req, res) => {
     const { CourseId, lessonTitle, lecture } = req.body;
     const newLesson = await db.Lesson.create({
@@ -20,10 +24,40 @@ module.exports = app => {
       UserId: req.user.id
     });
 
-    // console.log(newCourse);
-
     try {
       res.send(newLesson);
+    } catch (error) {
+      res.status(401).json(error);
+    }
+  });
+
+  // PUT Route for updating the user's lesson.
+  app.put("/api/lessons/:id", async (req, res) => {
+    const { CourseId, lessonTitle, lecture } = req.body;
+    const updatedInfo = {
+      CourseId,
+      lessonTitle,
+      lecture
+    };
+    const updatedLesson = await db.Lesson.create(updatedInfo, {
+      where: { id: req.params.id }
+    });
+
+    try {
+      res.json(updatedLesson);
+    } catch (error) {
+      res.status(401).json(error);
+    }
+  });
+
+  // DELETE Route for deleting a user's lesson.
+  app.delete("/api/lessons/:id", async (req, res) => {
+    const deleteLesson = await db.Lesson.destroy({
+      where: { id: req.params.id }
+    });
+
+    try {
+      res.json(deleteLesson);
     } catch (error) {
       res.status(401).json(error);
     }
