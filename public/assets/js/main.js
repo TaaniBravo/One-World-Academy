93ba618f711fd2bd03d8272bf397f059345cea01
Abdulhakeem Dahir <somlutions@gmail.com>
2021-01-31 13:00:54 -0800

    added comments

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(() => {
   $(".profile-pic").on("click", event => {
     event.preventDefault();
-
+    // This sends the user to the /user route
     window.location.replace("/user");
   });
 });
